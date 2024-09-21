ef1de90d8b897587f4509caec364bfd6a9cdfdbc
Rafajjj <abdullayevrafael@gmail.com>
2024-09-21 13:22:48 +0400

    First comment

---
+++ StudentInfoTask1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentInfoTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Rafael Abdullayev", 35);
+            student.GroupNo = 12;
+            student.Birthday = DateTime.Now.AddYears(-22);
+            student.ShowInfo();
+
+
+        }
+    }
+
+}
---
+++ StudentInfoTask1/Student.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace StudentInfoTask1
+{
+    public class Student
+    {
+        //fields
+        public string FullName;
+        public DateTime Birthday;
+        public int GroupNo;
+        public int Point;
+
+        public Student(string fullName, int point)
+        {
+            FullName = fullName;
+            Point = point;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Fullname: {FullName}  Birthday: {Birthday} GroupNo: {GroupNo} Point: {Point}");
+
+        }
+    }
+
+}
